830490c9878e590e83e23e0500ae1d66ccddcb57
Jason Q Bradley <98352141+Bradlj03@users.noreply.github.com>
2022-02-03 15:14:42 -0500

    added java

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log("hello world");
+
+
+console.log("HH  HH         111 111                                   111       d  !!! ");
+console.log("HH  HH    eee  111 111  oooo   ww      ww  oooo  rr rr   111       dd !!!  ");
+console.log("HHHHHH ee  ee  111 111 oo  oo  ww      ww oo  oo rrr  r  111   dddddd !!!     ");
+console.log("HH  HH eeeeee  111 111 oo  oo   ww ww ww  oo  oo rr      111 dd    dd !!!   ");
+console.log("HH  HH  eeee   111 111   oo      ww  ww    oooo  rr      111   dddddd !!!  ");
